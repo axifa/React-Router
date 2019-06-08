860be2de9f3a6eb381210b67f223446896c3a78a
Asifa Khan <[asifakhan00011@gmail.com]>
2019-06-07 23:04:00 -0400

    changes were made

---
+++ src/Home.js
@@ -0,0 +1,18 @@
+
+import logo from './logo.svg';
+import './App.css';
+
+import React, { Component } from 'react';
+    
+class Home extends Component {
+  render() {
+    return (
+        <div>
+          <img src="https://cache-content.credit.com/wp-content/uploads/2016/11/opening-bank-account-612x281.jpg" alt="bank"/>
+          <h1>Bank of React</h1>
+        </div>
+    );
+  }
+}
+
+export default Home;
